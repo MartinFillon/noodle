a36d6c48a942e65b5cc5119aa67b89c60b611378
MartinFillon <martin.fillon@hotmail.com>
2025-06-22 00:10:50 +0200

    feat(json): start basic json parsing

--- src/Noodle/Json.hs
+++ src/Noodle/Json.hs
@@ -1,7 +1,21 @@
-module Noodle.Json (Json (..), prettyPrintJson) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+
+module Noodle.Json (Json (..), prettyPrintJson, parseJson, parseValue) where
 
 import Data.List (intercalate)
+import Noodle.Parser.Utils (
+    Parser,
+    ParserError,
+    lexeme,
+    parseBool,
+    parseNumber,
+    parseString,
+    sc,
+ )
 import Noodle.Serializer (Serializer (..))
+import Text.Megaparsec (MonadParsec (eof), between, choice, parse, sepBy)
+import Text.Megaparsec.Char as MC
 
 data Json
     = JObject [(String, Json)]
@@ -63,3 +77,49 @@ merge' (JObject x) (JObject y) = JObject (x ++ y)
 merge' (JArray x) (JArray y) = JArray (x ++ y)
 merge' (JArray x) y = JArray (y : x)
 merge' x y = JArray [x, y]
+
+parseJsonNumber :: Parser Json
+parseJsonNumber = lexeme $ JNumber <$> parseNumber
+
+parseJsonString :: Parser Json
+parseJsonString = lexeme $ JString <$> parseString
+
+parseJsonNull :: Parser Json
+parseJsonNull = lexeme $ MC.string "null" >> return Null
+
+parseJsonBool :: Parser Json
+parseJsonBool = lexeme $ JBool <$> parseBool
+
+parseArray :: Parser Json
+parseArray =
+    JArray
+        <$> lexeme
+            ( between (char '[') (char ']') (parseValue `sepBy` lexeme ",")
+            )
+
+parseObjectItem :: Parser (String, Json)
+parseObjectItem =
+    lexeme (parseString >>= (\n -> lexeme $ char ':' >> parseObjectValue n))
+
+parseObjectValue :: String -> Parser (String, Json)
+parseObjectValue n = lexeme $ (n,) <$> lexeme parseValue
+
+parseObject :: Parser Json
+parseObject =
+    JObject
+        <$> lexeme (between (char '{') (char '}') (parseObjectItem `sepBy` lexeme ","))
+
+parseValue :: Parser Json
+parseValue =
+    lexeme $
+        choice
+            [ parseObject,
+              parseArray,
+              parseJsonNumber,
+              parseJsonString,
+              parseJsonNull,
+              parseJsonBool
+            ]
+
+parseJson :: String -> Either ParserError Json
+parseJson = parse (between sc eof parseValue) ""
---
+++ src/Noodle/Parser/Utils.hs
@@ -0,0 +1,73 @@
+module Noodle.Parser.Utils (
+    Parser,
+    sc,
+    sce,
+    lexeme,
+    parseNumber,
+    ParserError,
+    parseString,
+    parseBool,
+) where
+
+import Control.Applicative (Alternative (..))
+import Control.Monad (void)
+import Data.Void (Void)
+import Text.Megaparsec (
+    MonadParsec (..),
+    Parsec,
+    between,
+    choice,
+    noneOf,
+    (<?>),
+ )
+import Text.Megaparsec.Char (char, string)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Error (ParseErrorBundle)
+
+type Parser = Parsec Void String
+type ParserError = ParseErrorBundle String Void
+
+sc :: Parser ()
+sc =
+    L.space
+        (void $ some (char ' ' <|> char '\t' <|> char '\n' <|> char '\r'))
+        empty
+        empty
+
+sce :: Parser ()
+sce = L.space empty empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+parseNumber :: Parser Double
+parseNumber = L.signed sce L.float
+
+parseString :: Parser String
+parseString =
+    between
+        (char '\"')
+        (char '\"')
+        ( (:)
+            <$> (try parseEscapedChar <|> noneOf ("\"" :: [Char]))
+            <*> many (try parseEscapedChar <|> noneOf ("\"" :: [Char]))
+        )
+
+parseEscapedChar :: Parser Char
+parseEscapedChar =
+    choice
+        [ try (string "\\\"" >> return '\"'),
+          try (string "\\n" >> return '\n'),
+          try (string "\\r" >> return '\r'),
+          try (string "\\t" >> return '\t'),
+          try (string "\\\\" >> return '\\')
+        ]
+
+parseTrue :: Parser Bool
+parseTrue = lexeme $ string "true" >> return True
+
+parseFalse :: Parser Bool
+parseFalse = lexeme $ string "false" >> return False
+
+parseBool :: Parser Bool
+parseBool = lexeme (choice [parseTrue, parseFalse]) <?> "Boolean"
